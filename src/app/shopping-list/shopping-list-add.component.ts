00c9198fd0df13882f8b86ae69175ea836afb7c8
Geraldine <geraldine@g-angle.co.jp>
2017-02-09 14:11:24 +0800

    Add delete/clear form

--- src/app/shopping-list/shopping-list-add.component.ts
+++ src/app/shopping-list/shopping-list-add.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnChanges, Input } from '@angular/core';
+import { Component, OnChanges, Input, Output, EventEmitter } from '@angular/core';
 
 import { Ingredient } from '../shared/ingredient';
 import { ShoppingListService } from './shopping-list.service';
@@ -9,6 +9,7 @@ import { ShoppingListService } from './shopping-list.service';
 })
 export class ShoppingListAddComponent implements OnChanges {
   @Input() item: Ingredient;
+  @Output() cleared = new EventEmitter();
   isAdd = true;
 
   constructor(private sls: ShoppingListService) { }
@@ -26,10 +27,21 @@ export class ShoppingListAddComponent implements OnChanges {
     const newIngredient = new Ingredient(ingredient.name, ingredient.amount);
     if (!this.isAdd) {
       this.sls.editItem(this.item, newIngredient);
+      this.onClear();
     }else {
       this.item = newIngredient;
       this.sls.addItem(this.item);
     }
+  }
+
+  onDelete() {
+    this.sls.deleteItem(this.item);
+    this.onClear();
+  }
+
+  onClear() {
+    this.isAdd = true;
+    this.cleared.emit(null);
   }
 
 }
